dd1998607212500d6fb84287a08e29709a73d3ba
DiggiDon <aleksey230997@gmail.com>
2018-08-05 16:07:57 +0300

    add preprocess with ORB and pdist

--- video_feature_classification/main.py
+++ video_feature_classification/main.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
-import os, sys
-import cv2
+import os
+import sys
+import cv2 as cv
 import random
 import numpy as np
 from tqdm import tqdm
 import pickle
 
 sys.path.append('../')
+sys.path.append(os.getcwd())
 from pytorch.common.datasets_parsers.av_parser import AVDBParser
 
 from sklearn import svm
+from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.decomposition import PCA
 from accuracy import Accuracy
+
+import scipy as sc
+from time import time
 
 
 def get_data(dataset_root, file_list, max_num_clips=0, max_num_samples=50):
@@ -25,8 +31,11 @@ def get_data(dataset_root, file_list, max_num_clips=0, max_num_samples=50):
     print('frames count:', dataset_parser.get_dataset_size())
     return data
 
+
 def calc_features(data):
-    orb = cv2.ORB_create()
+    radius = 5
+    padding = radius * 2
+    orb = cv.ORB_create(edgeThreshold=0)
 
     progresser = tqdm(iterable=range(0, len(data)),
                       desc='calc video features',
@@ -36,19 +45,23 @@ def calc_features(data):
     feat, targets = [], []
     for i in progresser:
         clip = data[i]
-
-        rm_list = []
         for sample in clip.data_samples:
-            pass
+            image = np.pad(sample.image, (padding * 2,),
+                           'constant', constant_value=0)
+            kps = []
+            for landmark in sample.landmarks:
+                kps.append(cv.KeyPoint(
+                    landmark[0] + padding, landmark[1] + padding, radius))
+            kp, descr = orb.detect(image, kps)
 
-            # TODO: придумайте способы вычисления признаков по изображению с использованием ключевых точек
-            # используйте библиотеку OpenCV
+            pwdist = sc.spatial.distance.pdist(np.asarray(sample.landmarks))
+            feat.append(np.hstack((descr.ravel(), pwdist.ravel())))
 
-        for sample in rm_list:
-            clip.data_samples.remove(sample)
+            targets.append(clip.labels)
 
     print('feat count:', len(feat))
     return np.asarray(feat, dtype=np.float32), np.asarray(targets, dtype=np.float32)
+
 
 def classification(X_train, X_test, y_train, y_test, accuracy_fn, pca_dim):
     if pca_dim > 0:
@@ -68,27 +81,33 @@ def classification(X_train, X_test, y_train, y_test, accuracy_fn, pca_dim):
 
 
 if __name__ == "__main__":
-    experiment_name = 'exp_1'
-    max_num_clips = 0 # загружайте только часть данных для отладки кода
-    use_dump = False # используйте dump для быстрой загрузки рассчитанных фич из файла
+    experiment_name = 'ORB_not_sep_rad5_pdist'
+    max_num_clips = 0  # загружайте только часть данных для отладки кода
+    use_dump = False  # используйте dump для быстрой загрузки рассчитанных фич из файла
 
     # dataset dir
-    base_dir = 'D:/AVER'
+    base_dir = 'C:/Files/Datasets/SummerSchool STC'
     if 1:
-        train_dataset_root = base_dir + '/Ryerson/Speech/Video'
-        train_file_list = base_dir + '/Ryerson/Speech/train_data_with_landmarks.txt'
-        test_dataset_root = base_dir + '/Ryerson/Speech/Video'
-        test_file_list = base_dir + '/Ryerson/Speech/test_data_with_landmarks.txt'
+        train_dataset_root = base_dir + '/Ryerson/Video'
+        train_file_list = base_dir + '/Ryerson/train_data_with_landmarks.txt'
+        test_dataset_root = base_dir + '/Ryerson/Video'
+        test_file_list = base_dir + '/Ryerson/test_data_with_landmarks.txt'
     elif 1:
-        train_dataset_root = base_dir + '/OMGEmotionChallenge-master/omg_TrainVideos/preproc/frames'
-        train_file_list = base_dir + '/OMGEmotionChallenge-master/omg_TrainVideos/preproc/train_data_with_landmarks.txt'
-        test_dataset_root =base_dir + '/OMGEmotionChallenge-master/omg_ValidVideos/preproc/frames'
-        test_file_list = base_dir + '/OMGEmotionChallenge-master/omg_ValidVideos/preproc/valid_data_with_landmarks.txt'
+        train_dataset_root = base_dir + \
+            '/OMGEmotionChallenge-master/omg_TrainVideos/preproc/frames'
+        train_file_list = base_dir + \
+            '/OMGEmotionChallenge-master/omg_TrainVideos/preproc/train_data_with_landmarks.txt'
+        test_dataset_root = base_dir + \
+            '/OMGEmotionChallenge-master/omg_ValidVideos/preproc/frames'
+        test_file_list = base_dir + \
+            '/OMGEmotionChallenge-master/omg_ValidVideos/preproc/valid_data_with_landmarks.txt'
 
     if not use_dump:
         # load dataset
-        train_data = get_data(train_dataset_root, train_file_list, max_num_clips=0)
-        test_data = get_data(test_dataset_root, test_file_list, max_num_clips=0)
+        train_data = get_data(train_dataset_root,
+                              train_file_list, max_num_clips=0)
+        test_data = get_data(
+            test_dataset_root, test_file_list, max_num_clips=0)
 
         # get features
         train_feat, train_targets = calc_features(train_data)
@@ -96,11 +115,13 @@ if __name__ == "__main__":
 
         accuracy_fn = Accuracy(test_data, experiment_name=experiment_name)
 
-        #with open(experiment_name + '.pickle', 'wb') as f:
+        # with open(experiment_name + '.pickle', 'wb') as f:
         #    pickle.dump([train_feat, train_targets, test_feat, test_targets, accuracy_fn], f, protocol=2)
     else:
         with open(experiment_name + '.pickle', 'rb') as f:
-            train_feat, train_targets, test_feat, test_targets, accuracy_fn = pickle.load(f)
+            train_feat, train_targets, test_feat, test_targets, accuracy_fn = pickle.load(
+                f)
 
     # run classifiers
-    classification(train_feat, test_feat, train_targets, test_targets, accuracy_fn=accuracy_fn, pca_dim=100)
+    classification(train_feat, test_feat, train_targets,
+                   test_targets, accuracy_fn=accuracy_fn, pca_dim=100)
